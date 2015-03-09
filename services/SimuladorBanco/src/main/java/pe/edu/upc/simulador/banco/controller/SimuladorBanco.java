5307fc1e3f785a62fcb2e9ff81b5caea26279619
Danielle Delgado <danieldelgado20g@gmail.com>
2015-03-08 22:32:29 -0500

    solo queda 30%

--- services/SimuladorBanco/src/main/java/pe/edu/upc/simulador/banco/controller/SimuladorBanco.java
+++ services/SimuladorBanco/src/main/java/pe/edu/upc/simulador/banco/controller/SimuladorBanco.java
@@ -2,38 +2,62 @@ package pe.edu.upc.simulador.banco.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import pe.edu.upc.simulador.banco.bean.Tarjeta;
+import pe.edu.upc.simulador.banco.data.DataFile;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class SimuladorBanco {
-	
-	private static final Logger logger = LoggerFactory.getLogger(SimuladorBanco.class);
-	
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(SimuladorBanco.class);
+
+	@Autowired
+	private DataFile datafile;
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
 		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG,
+				DateFormat.LONG, locale);
+
 		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+
+		model.addAttribute("serverTime", formattedDate);
+
 		return "home";
 	}
-	
+
+	@RequestMapping(value = "/tarjetas", method = RequestMethod.GET)
+	public @ResponseBody List<Tarjeta> listarTarjetas() {
+		return datafile.getTarjetas();
+	}
+
+	@RequestMapping(value = "/tarjetas", method = RequestMethod.POST)
+	public @ResponseBody List<Tarjeta> validarTarjeta(String tipoTrajeta,
+			String numero_cuenta, String fechaVencimineto, String codSeguridad,
+			String clave, String dni) {
+		return datafile.validarTarjeta(tipoTrajeta,codSeguridad,numero_cuenta,fechaVencimineto,codSeguridad,clave,dni);
+	}
+
 }
--- services/SimuladorBanco/src/main/java/pe/edu/upc/simulador/banco/data/DataFile.java
+++ services/SimuladorBanco/src/main/java/pe/edu/upc/simulador/banco/data/DataFile.java
@@ -1,7 +1,11 @@
 package pe.edu.upc.simulador.banco.data;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import pe.edu.upc.simulador.banco.bean.Tarjeta;
 
 @Component
 public class DataFile {
@@ -11,6 +15,17 @@ public class DataFile {
 
 	@Autowired
 	private DataInstance dataInstance;
+
+	public List<Tarjeta> getTarjetas() {
+		return dataInstance.listarTarjetas();
+	}
+
+	public List<Tarjeta> validarTarjeta(String tipoTrajeta,
+			String codSeguridad, String numero_cuenta, String fechaVencimineto,
+			String codSeguridad2, String clave, String dni) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	
 	
