7a2290c5cabc26150b94d243a5e2a8e32c77652a
danieldelgado <danieldelgado20g@gmail.com>
2015-03-23 21:35:03 -0500

    gcmtest

---
+++ android/Test/app/src/main/java/pe/edu/upc/util/ServerUtilities.java
@@ -0,0 +1,128 @@
+
+package pe.edu.upc.util;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Random;
+
+import android.content.Context;
+import android.util.Log;
+
+/**
+ * Helper class used to communicate with the demo server.
+ */
+public final class ServerUtilities {
+
+    private static final int MAX_ATTEMPTS = 5;
+    private static final int BACKOFF_MILLI_SECONDS = 2000;
+    private static final Random random = new Random();
+
+    static final String TAG = "GCM Demo";
+    /**
+     * Register this account/device pair within the server.
+     *
+     */
+    public static void register(final Context context, final String regId) {
+        Log.i(TAG, "registering device (regId = " + regId + ")");
+        String serverUrl = CommonUtilities.SERVER_URL + "/register";
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("regId", regId);
+        try{
+
+            post(serverUrl, params);
+
+        }catch (IOException e){
+
+        }
+
+       // String message = context.getString(R.string.server_register_error,    MAX_ATTEMPTS);
+        //CommonUtilities.displayMessage(context, message);
+    }
+
+    /**
+     * Unregister this account/device pair within the server.
+     */
+    public static void unregister(final Context context, final String regId) {
+        Log.i(TAG, "unregistering device (regId = " + regId + ")");
+        String serverUrl = CommonUtilities.SERVER_URL + "/unregister";
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("regId", regId);
+        try {
+            post(serverUrl, params);
+           /* GCMRegistrar.setRegisteredOnServer(context, false);
+            String message = context.getString(R.string.server_unregistered);
+            CommonUtilities.displayMessage(context, message);*/
+        } catch (IOException e) {
+            // At this point the device is unregistered from GCM, but still
+            // registered in the server.
+            // We could try to unregister again, but it is not necessary:
+            // if the server tries to send a message to the device, it will get
+            // a "NotRegistered" error message and should unregister the device.
+           /* String message = context.getString(R.string.server_unregister_error,
+                    e.getMessage());
+            CommonUtilities.displayMessage(context, message);*/
+        }
+    }
+
+    /**
+     * Issue a POST request to the server.
+     *
+     * @param endpoint POST address.
+     * @param params request parameters.
+     *
+     * @throws IOException propagated from POST.
+     */
+    private static void post(String endpoint, Map<String, String> params)
+            throws IOException {
+        URL url;
+        try {
+            url = new URL(endpoint);
+        } catch (MalformedURLException e) {
+            throw new IllegalArgumentException("invalid url: " + endpoint);
+        }
+        StringBuilder bodyBuilder = new StringBuilder();
+        Iterator<Entry<String, String>> iterator = params.entrySet().iterator();
+        // constructs the POST body using the parameters
+        while (iterator.hasNext()) {
+            Entry<String, String> param = iterator.next();
+            bodyBuilder.append(param.getKey()).append('=')
+                    .append(param.getValue());
+            if (iterator.hasNext()) {
+                bodyBuilder.append('&');
+            }
+        }
+        String body = bodyBuilder.toString();
+        Log.v(TAG, "Posting '" + body + "' to " + url);
+        byte[] bytes = body.getBytes();
+        HttpURLConnection conn = null;
+        try {
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setUseCaches(false);
+            conn.setFixedLengthStreamingMode(bytes.length);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type",
+                    "application/x-www-form-urlencoded;charset=UTF-8");
+            // post the request
+            OutputStream out = conn.getOutputStream();
+            out.write(bytes);
+            out.close();
+            // handle the response
+            int status = conn.getResponseCode();
+            if (status != 200) {
+              throw new IOException("Post failed with error code " + status);
+            }
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+      }
+}
